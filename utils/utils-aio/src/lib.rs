6c65591b76ff272707ad982400319aabde818d7b
sinu <65924192+sinui0@users.noreply.github.com>
2023-01-12 12:00:05 -0800

    Partition Workspaces

---
+++ utils/utils-aio/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod adaptive_barrier;
+#[cfg(feature = "codec")]
+pub mod codec;
+#[cfg(feature = "duplex")]
+pub mod duplex;
+pub mod expect_msg;
+pub mod factory;
+#[cfg(feature = "mux")]
+pub mod mux;
+
+pub trait Channel<T>: futures::Stream<Item = T> + futures::Sink<T> + Send + Unpin {}
+
+impl<T, U> Channel<T> for U where U: futures::Stream<Item = T> + futures::Sink<T> + Send + Unpin {}
