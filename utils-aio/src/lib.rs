6804821226ddf7b7ac58b0ca6e8ebfd98b844456
sinu <65924192+sinui0@users.noreply.github.com>
2023-07-06 12:49:03 -0700

    io provider

--- utils-aio/src/lib.rs
+++ utils-aio/src/lib.rs
@@ -11,3 +11,13 @@ pub mod mux;
 pub mod non_blocking_backend;
 pub mod sink;
 pub mod stream;
+
+/// Trait implemented by a type which can provide a duplex channel.
+#[cfg(feature = "duplex")]
+pub trait IoProvider<T> {
+    /// The type of the duplex channel.
+    type Duplex: duplex::Duplex<T>;
+
+    /// Provides a duplex channel.
+    fn provide_io(&mut self) -> &mut Self::Duplex;
+}
