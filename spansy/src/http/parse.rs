772e0d32a286c2bb75c12e6bf892f578cd778c03
lancenonce <lancendavis47@gmail.com>
2024-07-02 09:02:09 +0000

    init parse.rs with function sigs

---
+++ spansy/src/http/parse.rs
@@ -0,0 +1,10 @@
+// Imports
+
+// Parsing functions for Transfer-Encoding header types
+fn parse_chunked_body() {}
+
+fn parse_gzip_body() {}
+
+fn parse_deflate_body() {}
+
+fn parse_idenity_body() {}
