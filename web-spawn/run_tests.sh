78a9ae0f65aa8c9e043fb0b24b9bbcd8c46cf3a3
sinu.eth <65924192+sinui0@users.noreply.github.com>
2025-03-07 10:45:38 -0800

    refactor(web-spawn): use snippets (#52)

--- web-spawn/run_tests.sh
+++ web-spawn/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Test from main browser thread
-WASM_BINDGEN_USE_BROWSER=1 wasm-pack test --headless --chrome --firefox
+WASM_BINDGEN_USE_BROWSER=1 wasm-pack test --headless --chrome --firefox -- --features no-bundler
 
 # Test from worker thread
-WASM_BINDGEN_USE_DEDICATED_WORKER=1 wasm-pack test --headless --chrome --firefox
+WASM_BINDGEN_USE_DEDICATED_WORKER=1 wasm-pack test --headless --chrome --firefox -- --features no-bundler
