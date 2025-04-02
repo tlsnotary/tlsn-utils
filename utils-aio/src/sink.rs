1b8dc43dd63d2bd174549a6ecca2f29c30a5bbb9
sinui0 <65924192+sinui0@users.noreply.github.com>
2025-04-02 11:11:25 +0700

    refactor: remove utils-aio

--- utils-aio/src/sink.rs
+++
@@ -1,6 +0,0 @@
-use futures::Sink;
-
-/// A sink with `std::io::Error` as the error type.
-pub trait IoSink<T>: Sink<T, Error = std::io::Error> {}
-
-impl<T, U> IoSink<T> for U where U: Sink<T, Error = std::io::Error> {}
