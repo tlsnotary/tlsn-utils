2d807711968e3fc4981d06ae3bb14497234ff6bb
Justin Martin <justin@keyring.network>
2025-04-28 13:57:13 -0700

    Same for response

--- spansy/src/http/span.rs
+++ spansy/src/http/span.rs
@@ -270,22 +270,23 @@ fn response_body_len(response: &Response) -> Result<usize, ParseError> {
 
     if response
         .headers_with_name("Transfer-Encoding")
-        .any(|h| {
+        .next()
+        .map(|h| {
             std::str::from_utf8(h.value.0.as_bytes())
                 .unwrap_or("")
                 .split(',')
                 .any(|v| v.trim() != "identity")
         })
+        .unwrap_or(false)
     {
         let bad_values = response
             .headers_with_name("Transfer-Encoding")
-            .map(|h| std::str::from_utf8(h.value.0.as_bytes()).unwrap_or(""))
+            .map(|h| std::str::from_utf8(h.value.0.as_bytes()).unwrap_or("{invalid utf-8}"))
             .collect::<Vec<_>>()
             .join(", ");
 
         Err(ParseError(format!(
-            "Transfer-Encoding other than identity not supported yet: {:?}",
-            bad_values
+            "Transfer-Encoding other than identity not supported yet: {bad_values}"
         )))
     } else if let Some(h) = response.headers_with_name("Content-Length").next() {
         // If a valid Content-Length header field is present without Transfer-Encoding, its decimal value
