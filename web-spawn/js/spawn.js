f9b890a8a2667fec0db90da8c14dd69d1e3abcbb
sinu <65924192+sinui0@users.noreply.github.com>
2025-08-12 10:15:12 -0700

    simpler

--- web-spawn/js/spawn.js
+++ web-spawn/js/spawn.js
@@ -17,11 +17,13 @@ registerMessageListener(self, 'web_spawn_start_spawner', async (data) => {
     );
     const [module, memory, spawnerPtr] = data;
     const pkg = await import('../../..');
-    await pkg.default({ module, memory });
+    const exports = await pkg.default({ module, memory });
 
     const spawner = pkg.web_spawn_recover_spawner(spawnerPtr);
     postMessage('web_spawn_spawner_ready');
     await spawner.run(workerUrl.toString());
+
+    exports.__wbindgen_thread_destroy();
 
     close();
 });
@@ -31,11 +33,12 @@ registerMessageListener(self, 'web_spawn_start_worker', async (data) => {
     const [module, memory, workerPtr] = data;
 
     const pkg = await import('../../..');
-    await pkg.default({ module, memory });
+    const exports = await pkg.default({ module, memory });
 
     pkg.web_spawn_start_worker(workerPtr);
 
-    wasm.wasm_exports().__wbindgen_thread_destroy();
+    exports.__wbindgen_thread_destroy();
+
     close();
 });
 
