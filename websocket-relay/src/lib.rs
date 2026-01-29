9b527a70c14b2c18822c6505925c2bcc87753e39
dan <themighty1@users.noreply.github.com>
2026-01-29 16:39:49 +0200

    feat(websocket-relay): add ability to bind to a local address

--- websocket-relay/src/lib.rs
+++ websocket-relay/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    net::SocketAddr,
+    net::{IpAddr, SocketAddr},
     sync::{
         Arc, Mutex,
         atomic::{AtomicBool, Ordering},
@@ -12,13 +12,86 @@ use futures::{SinkExt, StreamExt as _};
 use once_cell::sync::Lazy;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpSocket, TcpStream},
 };
 use tokio_tungstenite::{
     WebSocketStream, accept_hdr_async,
     tungstenite::{Message, http::Request},
 };
 use tracing::{debug, info, instrument};
+
+/// WebSocket relay server.
+#[derive(Debug, Clone, Default)]
+pub struct Relay {
+    /// Local address to bind outgoing TCP connections to.
+    ///
+    /// If set, the relay will bind to this address before connecting to the
+    /// target TCP server. This is useful for controlling the source IP address
+    /// of outgoing connections.
+    ///
+    /// If not set, the operating system will choose the source address based on
+    /// the routing table.
+    local_address: Option<IpAddr>,
+}
+
+impl Relay {
+    /// Creates a new relay builder.
+    pub fn builder() -> RelayBuilder {
+        RelayBuilder::default()
+    }
+
+    /// Runs the websocket relay server with the given TCP listener.
+    #[instrument(skip(self))]
+    pub async fn run(&self, listener: TcpListener) -> Result<()> {
+        loop {
+            let (socket, addr) = listener.accept().await?;
+            info!("accepted connection from: {}", addr);
+
+            let relay = self.clone();
+            tokio::spawn(async move { relay.handle_connection(addr, socket).await });
+        }
+    }
+
+    #[instrument(skip(self, io), err)]
+    async fn handle_connection(&self, addr: SocketAddr, io: TcpStream) -> Result<()> {
+        match accept_ws(io).await? {
+            Mode::Ws { id, ws } => {
+                tokio::spawn(handle_ws(id, ws));
+            }
+            Mode::Tcp { addr, ws } => {
+                let local_address = self.local_address;
+                tokio::spawn(handle_tcp(addr, local_address, ws));
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// Builder for [`Relay`].
+#[derive(Debug, Default)]
+pub struct RelayBuilder {
+    local_address: Option<IpAddr>,
+}
+
+impl RelayBuilder {
+    /// Sets the local address to bind outgoing TCP connections to.
+    ///
+    /// This controls the source IP address used when connecting to target TCP
+    /// servers. If not set, the operating system will choose the source address
+    /// based on the routing table.
+    pub fn local_address(mut self, addr: IpAddr) -> Self {
+        self.local_address = Some(addr);
+        self
+    }
+
+    /// Builds the relay.
+    pub fn build(self) -> Relay {
+        Relay {
+            local_address: self.local_address,
+        }
+    }
+}
 
 #[derive(Debug, Default)]
 struct State {
@@ -44,28 +117,12 @@ enum Mode {
 }
 
 /// Runs the websocket relay server with the given TCP listener.
+///
+/// This is a convenience function that creates a default [`Relay`] and runs it.
+/// For more control over the relay configuration, use [`Relay::builder()`].
 #[instrument]
 pub async fn run(listener: TcpListener) -> Result<()> {
-    loop {
-        let (socket, addr) = listener.accept().await?;
-        info!("accepted connection from: {}", addr);
-
-        tokio::spawn(handle_connection(addr, socket));
-    }
-}
-
-#[instrument(skip(io), err)]
-async fn handle_connection(addr: SocketAddr, io: TcpStream) -> Result<()> {
-    match accept_ws(io).await? {
-        Mode::Ws { id, ws } => {
-            tokio::spawn(handle_ws(id, ws));
-        }
-        Mode::Tcp { addr, ws } => {
-            tokio::spawn(handle_tcp(addr, ws));
-        }
-    }
-
-    Ok(())
+    Relay::default().run(listener).await
 }
 
 #[instrument(level = "debug", skip_all, err)]
@@ -146,8 +203,19 @@ async fn handle_ws(id: ConnectionId, ws: WebSocketStream<TcpStream>) -> Result<(
 
 /// Relays data between a websocket client and a TCP server.
 #[instrument(level = "debug", skip(ws), err)]
-async fn handle_tcp(addr: String, ws: WebSocketStream<TcpStream>) -> Result<()> {
-    let mut tcp = TcpStream::connect(addr).await?;
+async fn handle_tcp(
+    addr: String,
+    local_address: Option<IpAddr>,
+    ws: WebSocketStream<TcpStream>,
+) -> Result<()> {
+    let mut tcp = match local_address {
+        Some(local_addr) => {
+            let socket = TcpSocket::new_v4()?;
+            socket.bind(SocketAddr::new(local_addr, 0))?;
+            socket.connect(addr.parse()?).await?
+        }
+        None => TcpStream::connect(addr).await?,
+    };
     tcp.set_nodelay(true)?;
 
     let (mut sink, mut stream) = ws.split();
