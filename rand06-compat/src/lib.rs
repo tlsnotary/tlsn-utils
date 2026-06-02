ab8d131fcb91d7149a2ed770b6839f2b752392f4
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2026-06-02 15:49:17 +0200

    fmt

--- rand06-compat/src/lib.rs
+++ rand06-compat/src/lib.rs
@@ -89,10 +89,8 @@ where
     }
 }
 
-impl<R> rand_core_06::CryptoRng for Rand0_6CompatWrapper<R> where
-    R: rand_core::TryCryptoRng + ?Sized
-{
-}
+impl<R> rand_core_06::CryptoRng for Rand0_6CompatWrapper<R> where R: rand_core::TryCryptoRng + ?Sized
+{}
 
 #[cfg(test)]
 mod tests {
